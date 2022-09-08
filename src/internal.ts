9bd6bc32a4d749a8a8325b68619d43648a706b86
Anthony Ivanov <aivanov@swiftinvention.com>
2022-09-08 12:37:47 +0200

    sdk for demo

---
+++ src/internal.ts
@@ -0,0 +1,57 @@
+import meeLogo from '../assets/meeLogo.svg';
+
+export const goToMee = async (partnerId: string) => {
+  window.location.href = `https://getmee.org/#/consent/${partnerId}`;
+};
+
+const textColor = '#111827';
+
+export const createButton = (config: {
+  partnerId: string,
+  containerId: string,
+  classNames?: { text?: string, logo?: string, button?: string }
+}) => {
+  const container = document.getElementById(config.containerId);
+  const button = document.createElement('button');
+  const logo = document.createElement('img');
+  logo.src = meeLogo;
+  logo.alt = 'Mee';
+  logo.style.width = '18px';
+  logo.style.height = '18px';
+  logo.style.paddingTop = '10px';
+  logo.style.paddingBottom = '10px';
+  const text = document.createElement('p');
+  logo.className = config.classNames?.logo || 'meeLogo';
+  text.className = config.classNames?.text || 'meeText';
+  button.className = config.classNames?.button || 'meeButton';
+  text.innerHTML = 'Connect with Mee';
+  text.style.fontWeight = '700';
+  text.style.margin = '0px';
+  text.style.fontSize = '14px';
+  text.style.color = textColor;
+  button.style.display = 'flex';
+  button.style.flexDirection = 'row';
+  button.style.alignItems = 'center';
+  button.style.justifyContent = 'center';
+  button.style.width = '100%';
+  button.style.gap = '8px';
+  button.style.paddingLeft = '8px';
+  button.style.paddingRight = '16px';
+  button.style.width = '170px';
+  button.style.borderRadius = '3px';
+  button.style.border = '0px solid #808080';
+  button.style.backgroundColor = '#FFF';
+  button.addEventListener('mouseover', () => {
+    button.style.boxShadow = '0px 4px 6px -2px #0000000D, 0px 10px 15px -3px #0000001A';
+    return undefined;
+  });
+  button.addEventListener('mouseleave', () => {
+    button.style.boxShadow = '';
+    return undefined;
+  });
+  button.addEventListener('click', () => goToMee(config.partnerId));
+
+  button.appendChild(logo);
+  button.appendChild(text);
+  container?.appendChild(button);
+};
--- src/mee-sdk.ts
+++ src/mee-sdk.ts
@@ -1,1 +1,17 @@
-export const helloMee = () => 'Hello Mee';
+import { createButton, goToMee } from './internal';
+
+export const buttonAction = (partnerId: string) => {
+  goToMee(partnerId);
+};
+
+export const initButton = (config: {
+  partnerId: string,
+  containerId: string,
+  classNames?: { text?: string, logo?: string, button?: string }
+}) => {
+  createButton({
+    partnerId: config.partnerId,
+    containerId: config.containerId,
+    classNames: config.classNames,
+  });
+};
